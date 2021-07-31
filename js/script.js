be6bb75385bc2f01204c963e4afbd354fef00eb7
Joaquin Cavitelli <65786138+Hiperseco@users.noreply.github.com>
2021-07-31 11:49:04 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
  var search = document.getElementById("search")
  var logoimg = document.getElementById("logoimg")
 
-body.Element.requestFullscreen();
+body.requestFullscreen();
 
 
  iSearch.addEventListener("click", function () {
